10ee7db82eb947483bc9b6443b0bab20b5018d88
tonnyvijayan <tonnyvijayan@gmail.com>
2024-07-11 16:55:06 +0530

    fix:mobile [200~responsiveness

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-var buttonTranslate=document.querySelector('#button-Translate');
-var textArea=document.querySelector('#text-area');
-var outputdiv=document.querySelector('#output');
+const buttonTranslate = document.querySelector("#button-Translate");
+const textArea = document.querySelector("#text-area");
+const outputdiv = document.querySelector("#output");
 
-var serverUrl="https://api.funtranslations.com/translate/shakespeare.json"
+const serverUrl = "https://api.funtranslations.com/translate/shakespeare.json";
 
-function makeurl(text){
-    
-    return serverUrl+"?text="+text
-};
-function errorHandler(error){
-    console.log("Error occured", error)
-    alert("Unexpected error detected, Please try again after sometime")
-};
-
-
-function clickhandler(){
-    var textAreaValue=textArea.value;
-    fetch(makeurl(textAreaValue))
-         .then(response=>response.json())
-         .then(json=>{
-             var translatedText=json.contents.translated;
-             outputdiv.innerText=translatedText;}) 
-         .catch(errorHandler)    
+function makeurl(text) {
+  return serverUrl + "?text=" + text;
 }
-    
-   
 
+function errorHandler(error) {
+  console.log("Error occured", error);
+  alert("Unexpected error detected, Please try again after sometime");
+}
 
-buttonTranslate.addEventListener('click',clickhandler)
+function clickhandler() {
+  const textAreaValue = textArea.value;
+  fetch(makeurl(textAreaValue))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputdiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+buttonTranslate.addEventListener("click", clickhandler);
